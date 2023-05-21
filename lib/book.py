17989559b8eab4eca8d7ee196a43b4860f6b4c8c
BETHWEL <bethwelk85@gmail.com>
2023-05-21 20:22:00 +0300

    done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,19 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count, publication_year=None):
+        self.title = title
+        self.page_count = page_count
+        self.publication_year = publication_year
+
+    def get_title(self):
+        return self.title
+
+    def get_page_count(self):
+        return self.page_count
+
+    def get_publication_year(self):
+        return self.publication_year
+
+
+
+
+
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, color, size):
+        self.brand = brand
+        self.color = color
+        self.size = size
+
+    def get_brand(self):
+        return self.brand
+
+    def get_color(self):
+        return self.color
+
+    def get_size(self):
+        return self.size
